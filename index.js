50796bfc105faac1f07b87d7283575911881e015
ROHITH1709-byte <24bct140@kgcas.com>
2025-07-12 10:45:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var number=12;
+var name='js';
+var iscloudy=true;
+
+console.log(number+" "+name+" "+iscloudy);
+document.write(number+" "+name+" "+iscloudy);
+
+//TASK 2
+console.log(typeof{number});
+console.log(typeof{name});
+console.log(typeof{iscloudy});
+
+//task3
+
+var number1=prompt("enter the value of number1");
+var number2=prompt("enter the value of number2");
+console.log(number1+number2);
+console.log(number1*number2);
+console.log(number1-number2);
+console.log(number1/number2);
